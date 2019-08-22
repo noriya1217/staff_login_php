f76ddbb4c2155ac986126e730900ffb86c31a370
noriya <noriya.saeki@gmail.com>
2019-08-22 22:40:49 +0900

    関数共通化

--- staff_top.php
+++ staff_top.php
@@ -19,6 +19,7 @@
     <body>
         ショップ管理トップメニュー<br><br>
         <a href="../staff/staff_list.php">スタッフ管理</a><br><br>
-        <a href="../product/pro_list.php">商品管理</a>
+        <a href="../product/pro_list.php">商品管理</a><br><br>
+        <a href='staff_logout.php'>ログアウト</a>
     </body>
 </html>
