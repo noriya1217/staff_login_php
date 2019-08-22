200f075d333003ae9b93b64f034a8090ed6feebe
noriya <noriya.saeki@gmail.com>
2019-08-22 11:41:38 +0900

    loginチェック処理追加

---
+++ staff_top.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+    session_regenerate_id(true);
+    if (isset($_SESSION['login']) == false) {
+        echo 'ログインされていません<br>';
+        print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
+        exit();
+    } else {
+        echo $_SESSION['staff_name'].'さんログイン中<br><br>';
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>ろくまる農園</title>
+    </head>
+    <body>
+        ショップ管理トップメニュー<br><br>
+        <a href="../staff/staff_list.php">スタッフ管理</a><br><br>
+        <a href="../product/pro_list.php">商品管理</a>
+    </body>
+</html>
